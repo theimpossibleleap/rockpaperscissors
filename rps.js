bfcc170140dbc32a2948489ca4c1720f16fb4b56
theimpossibleleap <122632859+theimpossibleleap@users.noreply.github.com>
2023-02-14 15:07:12 -0800

    added documentation and new functions

--- rps.js
+++ rps.js
@@ -1,7 +1,26 @@
-const gameChoices = ["Rock", "Paper", "Scissors"];
+// An array for all choices available to the computer and the player.
+const gameChoices = ["rock", "paper", "scissors"];
 
+// A function to randomly assign a value from the array of choices to the computer selection.
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * gameChoices.length);
     return gameChoices[randomChoice];
 }
 
+// A function to get the player choice from a prompt, validate that it's either Rock, Paper, or Scissors, and return the value.
+function getPlayerChoice() {
+    // let playerChoice = promp("Choose one: Rock, Paper, or Scissors.");
+    
+
+}
+
+let computerSelection = getComputerChoice();
+let playerSelection = getPlayerChoice();
+
+console.log(computerSelection);
+
+function playRound(computerSelection, playerSelection) {
+    
+}
+
+//playRound();
