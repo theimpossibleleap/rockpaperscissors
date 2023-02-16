fa7de68dc00ba5da62efa5a7bddce39dc81cc10f
theimpossibleleap <122632859+theimpossibleleap@users.noreply.github.com>
2023-02-16 02:14:11 -0800

    Final changes..

--- rps.js
+++ rps.js
@@ -1,15 +1,22 @@
 // An array for all choices available to the computer and the player.
+
 const gameChoices = ["rock", "paper", "scissors"];
 let roundResults = [];
 let tieGame = [];
 
+
+
 // A function to randomly assign a value from the array of choices to the computer selection.
+
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * gameChoices.length);
     return gameChoices[randomChoice];
 }
 
+
+
 // A function to get the player choice from a prompt, validate that it's either Rock, Paper, or Scissors, and return the value.
+
 function getPlayerChoice() {
     let playerChoice = prompt(
         "Please make a choice: Rock, Paper, or Scissors."
@@ -18,7 +25,10 @@ function getPlayerChoice() {
     return !gameChoices.includes(playerChoice) ? getPlayerChoice(): playerChoice;
 }
 
+
+
 // A function to play a round and return the result to an array (win, lose, or tie).
+
 function playRound(compPlay, playerPlay) {
 
     console.log("You picked: " + compPlay);
@@ -47,7 +57,10 @@ function playRound(compPlay, playerPlay) {
     }
 }
 
+
+
 // A function to count the number of wins after reach round.
+
 function winCount() {
     let wins = 0;
 
@@ -60,7 +73,10 @@ function winCount() {
     return wins;
 }
 
+
+
 // A function to count the number of losses after reach round.
+
 function lossCount() {
     let losses = 0;
 
@@ -72,6 +88,8 @@ function lossCount() {
 
     return losses;
 }
+
+
 
 // A function that is called to accept the player input, generate the computers choice, play a round, count the number of wins and losses each roud, and once you either win 3 or lose 3, it shows you the winner. The game then restarts.
 
